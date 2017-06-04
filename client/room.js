08a9237cedb3542529289017bc16821524c3b4cd
johnmave126 <johnmave126@gmail.com>
2017-06-04 06:37:34 +0000

    First commit

---
+++ client/room.js
@@ -0,0 +1,63 @@
+import * as RLocalStorage from 'meteor/simply:reactive-local-storage'
+
+//TODO: Click on a tr to focus on it(stick it to top), can block creator after sticking
+
+Tracker.autorun(function() {
+    Meteor.subscribe("rooms", {
+        type: RLocalStorage.getItem('room_type'),
+        day: RLocalStorage.getItem('room_day')
+    });
+});
+    
+
+Template.body.helpers({
+    is_type(type) {
+        return type == (RLocalStorage.getItem('room_type') || 'all');
+    },
+    room_types() {
+        return [
+            {type: '7w', text: '7W房'},
+            {type: '12w', text: '12W房'},
+            {type: 'all', text: '所有房'}
+        ];
+    },
+    is_day(day) {
+        return day == (RLocalStorage.getItem('room_day') || 'all');
+    },
+    room_days() {
+        return [
+            {day: 'special', text: '曜日房'},
+            {day: 'normal', text: '普通房'},
+            {day: 'all', text: '所有房'}
+        ];
+    },
+    rooms() {
+        return Rooms.find({}, {sort: {time: -1}}).map(function(room) {
+            var body = room.body.trim();
+            var number = body.match(/\d\d\d\d\d/);
+            var type = body.match('ベテ') ? '12w' : '7w';
+            var cdate = new Date(Date.parse(room.created_at));
+            var date = cdate.toString().slice(16, 24);
+            return {creator_id: room.created_by_id, type: type, number: number, date: date, body: body};
+        });
+    }
+});
+
+Template.body.onRendered(function() {
+    $(document.body).on('change.tplbody', '#group_type', function(e) {
+        RLocalStorage.setItem('room_type', e.target.dataset.type);
+    });
+    $(document.body).on('change.tplbody', '#group_day', function(e) {
+        RLocalStorage.setItem('room_day', e.target.dataset.type);
+    });
+    if(!RLocalStorage.getItem('room_type')) {
+        RLocalStorage.setItem('room_type', 'all');
+    }
+    if(!RLocalStorage.getItem('room_day')) {
+        RLocalStorage.setItem('room_day', 'all');
+    }
+});
+
+Template.body.onCreated(function() {
+    $(document.body).off('.tplbody');
+});
---
+++ server/room.js
@@ -0,0 +1,96 @@
+Meteor.publish("rooms", function(filter) {
+    var selector = {};
+    switch(filter.type) {
+        case '7w':
+        case '12w':
+            selector.type = filter.type;
+            break;
+        case 'all':
+        default:
+            break;
+    }
+    switch(filter.day) {
+        case 'special':
+        case 'normal':
+            selector.day_special = filter.day == 'special';
+            break;
+        case 'all':
+        default:
+            break;
+    }
+    return Rooms.find(selector, {sort: {time: -1}, limit: 50});
+});
+
+function mutate(tweet) {
+    var body = tweet.text;
+    var ranges = [];
+    var i;
+    for(i = 0; i < tweet.entities.hashtags.length; i++) {
+        ranges.push(tweet.entities.hashtags[i].indices);
+    }
+    for(i = 0; i < tweet.entities.urls.length; i++) {
+        ranges.push(tweet.entities.urls[i].indices);
+    }
+    for(i = 0; i < tweet.entities.user_mentions.length; i++) {
+        ranges.push(tweet.entities.user_mentions[i].indices);
+    }
+    ranges.sort(function(a, b) {
+        return b[0] - a[0];
+    });
+    for(i = 0; i < ranges.length; i++) {
+        body = body.slice(0, ranges[i][0]) + body.slice(ranges[i][1]);
+    }
+    return body.replace(
+        /[\uff01-\uff5e]/g,
+        function(ch) {
+            return String.fromCharCode(ch.charCodeAt(0) - 0xfee0);
+        }
+    );
+}
+
+function make_room(tweet, text) {
+    return {
+        created_by_id: tweet.user.id_str,
+        created_by: '@' + tweet.user.screen_name,
+        created_at: tweet.created_at,
+        time: tweet.timestamp_ms,
+        body: text,
+        type: text.match('ベテ') ? '12w' : '7w',
+        day_special: !!text.match('曜')
+    };
+
+}
+
+function is_room(text) {
+    return text.match(/\d\d\d\d\d/);
+}
+
+Meteor.startup(function() {
+    var Twit = new TwitMaker({
+        consumer_key: Meteor.settings.consumer_key,
+        consumer_secret: Meteor.settings.consumer_secret,
+        access_token: Meteor.settings.access_token,
+        access_token_secret: Meteor.settings.access_token_secret
+    });
+
+    var stream = Twit.stream('statuses/filter', { track: ['#ガルパ協力', '#バンドリ協力'] });
+    stream.on('tweet', Meteor.bindEnvironment(function(tweet) {
+        var text = mutate(tweet);
+        if(is_room(text)) {
+            Rooms.insert(make_room(tweet, text));
+        }
+    }));
+
+    SyncedCron.add({
+        name: 'Delete old tweets',
+        schedule: function(parser) {
+            return parser.cron('8 0 * * ? *');
+        },
+        job: function() {
+            var now = new Date();
+            now.setDate(c.getDate() - 1);
+            Rooms.remove({time: {$lt: now.getTime()}});
+        }
+    });
+    SyncedCron.start();
+});
